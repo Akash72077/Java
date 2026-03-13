df2b5895eb0b8da48d963dfe80c69f2d4fdc9ab3
Akash72077 <sai.sai72077@gmail.com>
2026-03-13 17:35:39 +0530

    Completed upto insert at end , printing size

--- LinkedList/implementation.java
+++ LinkedList/implementation.java
@@ -1,7 +1,57 @@
 
 public class implementation {
     
+    public static class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+    }
+}
+
+public static class linkedlist{
+            Node head=null;
+            Node tail=null;
+            void insertAtEnd(int value){
+                Node temp=new Node(value);
+                if(head==null ){
+                    head=temp;
+                    tail=temp;
+                }
+                else{
+                tail.next=temp;
+                tail=temp;
+                }
+            }
+        void display(){
+            Node temp= head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+        }
+        int size(){
+            int count=0;
+             Node temp= head;
+            while(temp!=null){
+                count++;
+                temp=temp.next;
+            }
+            return count;
+
+        }
+
+}
     public static void main(String[] args) {
+        linkedlist ll= new linkedlist();
+        ll.insertAtEnd(5); // 5
+        ll.insertAtEnd(7); // 5 7
+        ll.insertAtEnd(8); // 5 7 8
+        ll.display(); 
+        System.out.println();
+       // System.out.println("Size of the LinkedList is: "+ll.size());
+
+        
         
     }
 }

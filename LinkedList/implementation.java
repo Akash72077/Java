429bc3fda562472a7b52fe5890db19664672c378
Akash72077 <sai.sai72077@gmail.com>
2026-03-13 18:33:16 +0530

    Completed function to insert element at at given position and get element by index or position

--- LinkedList/implementation.java
+++ LinkedList/implementation.java
@@ -51,22 +51,63 @@ public static class linkedlist{
                     head=temp;
                 }
         }
-}
+        void insertAt(int idx,int value){
+            if(idx==size()){  // if index postion is last then head will be not updated so we have to insert using insert at end function 
+
+                insertAtEnd(value);
+                return;
+            }
+            else if(idx==0){  // if index postion is first then head will be not updated so we have to insert using insert at begining function   
+                insertAtBeginning(value); 
+                return;
+            }
+            else if(idx<0 || idx>size()){
+                System.out.println("Wrong index");
+                return; 
+            }
+        Node t=new Node(value);
+        Node temp=head;
+        for(int i=1; i<=idx-1; i++){
+            temp=temp.next;
+        }
+        t.next=temp.next;
+        temp.next=t;
+    }
+
+        int getAt(int idx){
+            if(idx<0 || idx>size()){
+                System.out.println("Wrong index");
+                return -1; 
+            }
+            Node temp=head;
+            for(int i=1; i<=idx; i++){
+                temp=temp.next;
+            }
+            return temp.data;
+        }
+    }
     public static void main(String[] args) {
         linkedlist ll= new linkedlist();
         ll.insertAtEnd(5); // 5
         ll.insertAtEnd(7); // 5 7
         ll.insertAtEnd(8); // 5 7 8
-        ll.display(); 
-        System.out.println();
+       // ll.display(); 
+       // System.out.println();
        // System.out.println("Size of the LinkedList is: "+ll.size());
        ll.insertAtBeginning(6); // 6 5 7 8
        ll.insertAtBeginning(1); // 1 6 5 7 8
        ll.insertAtBeginning(2); // 2 1 6 5 7 8
        ll.insertAtBeginning(3); // 3 2 1 6 5 7 8
          ll.display(); 
-         System.out.println();
-         System.out.println("Size of the LinkedList is: "+ll.size());
+         //System.out.println();
+       //  System.out.println("Size of the LinkedList is: "+ll.size());
+       ll.insertAt(2,10); //3 2 10 1 6 5 7 8
+       System.out.println();
+       ll.display();
+       System.out.println();
+       System.out.println(ll.getAt(2));
+       
+
 
 
         
